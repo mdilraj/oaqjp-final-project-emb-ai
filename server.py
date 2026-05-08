b6f20c05c5340928c99ff45c2134517950ec2ebf
Mitika Dilraj <mitika.dilraj.168@gmail.com>
2026-05-07 17:28:03 -0700

    changes for pylint

--- server.py
+++ server.py
@@ -1,20 +1,30 @@
-from flask import Flask, request, json, render_template, redirect
+"""
+This is the server code for sentiment analysis
+"""
+from flask import Flask, request, render_template
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 app = Flask("Sentiment Analyzer")
-
 @app.route('/emotionDetector')
 def sent_analyzer():
+    """
+    This API calls the sentiment analyzer and returns the label 
+    and score to ui
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     result = sentiment_analyzer(text_to_analyze)
     if result['label']:
-        res = f"The given text has been identified as {result['label'][5:]} with a score of {result['score']}"
+        res = (f"The given text has been identified as {result['label'][5:]}"
+        f" with a score of {result['score']}")
     else:
         res = "Invalid input! Try again."
     return res
 
 @app.route('/')
 def render_index_page():
+    """
+    This method renders index page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
